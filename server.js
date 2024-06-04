9ce850c0c75a8bca0542113f08d7930f97bc1d7a
souravnmenon <souravnmenon@gmail.com>
2024-06-04 18:47:11 +0530

    Email Function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json())
 
 const { v4: uuidv4 } = require("uuid");
 
